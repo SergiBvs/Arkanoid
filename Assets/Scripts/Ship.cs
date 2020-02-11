6be9a650fb33249968e849c188f5209845970559
SergiBvs <sergi.barrachina@epiagranollers.cat>
2020-02-11 16:59:34 +0100

    Bounds parets
    Ficat els bounds a les parets perque la nau no es surti

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Ship : MonoBehaviour {
 
     public int ShipSpeed;
+    public GameObject RightWall;
+    public GameObject LeftWall;
+
 
 	void Start ()
     {
@@ -14,14 +17,24 @@ public class Ship : MonoBehaviour {
 	
 	void Update ()
     {
-		if(Input.GetAxisRaw("Horizontal") > 0 )//move right
+        if ((Input.GetAxisRaw("Horizontal") > 0) && (IntersectBoundsRight(GetComponent<SpriteRenderer>(), RightWall.GetComponent<SpriteRenderer>()) == false))//move right
         {
             this.transform.position += Vector3.right * Time.deltaTime * ShipSpeed;
         }
 
-        if(Input.GetAxisRaw("Horizontal")<0)// move left
+        if((Input.GetAxisRaw("Horizontal")<0) && (IntersectBoundsLeft(GetComponent<SpriteRenderer>(), LeftWall.GetComponent<SpriteRenderer>()) == false))// move left
         {
             this.transform.position += Vector3.left * Time.deltaTime * ShipSpeed;
         }
 	}
+
+    public bool IntersectBoundsRight(SpriteRenderer l_RightWall, SpriteRenderer l_Ship)
+    {
+        return l_RightWall.bounds.max.x > l_Ship.bounds.min.x && l_RightWall.bounds.min.x < l_Ship.bounds.max.x;
+    }
+
+    public bool IntersectBoundsLeft(SpriteRenderer l_LeftWall, SpriteRenderer l_Ship)
+    {
+        return l_LeftWall.bounds.max.x > l_Ship.bounds.min.x && l_LeftWall.bounds.min.x < l_Ship.bounds.max.x;
+    }
 }
