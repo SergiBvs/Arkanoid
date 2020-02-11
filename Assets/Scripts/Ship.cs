548f785ce8e32327d3ac190d64a86b60e97eb941
SergiBvs <sergi.barrachina@epiagranollers.cat>
2020-02-11 16:04:53 +0100

    initial commmit

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+    public int ShipSpeed;
+
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+		if(Input.GetAxisRaw("Horizontal") > 0 )//move right
+        {
+            this.transform.position += Vector3.right * Time.deltaTime * ShipSpeed;
+        }
+
+        if(Input.GetAxisRaw("Horizontal")<0)// move left
+        {
+            this.transform.position += Vector3.left * Time.deltaTime * ShipSpeed;
+        }
+	}
+}
